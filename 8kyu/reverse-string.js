15ebbdee71a822241de264ed5fa0965ee74fc289
Sharif New <Sharif@Sharifs-MacBook-Pro-2.local>
2022-09-07 14:33:54 -0700

    Daily exercises

---
+++ 8kyu/reverse-string.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//Solution:
+
+function solution(str) {
+    return str.split('').reverse().join('');
+
+}
