c7576b64721139b77999f84e1fa4adc54dff4157
sheriffisintown <shshibli97@gmail.com>
2022-10-08 14:58:08 -0700

    add

---
+++ October/10-08-2022.js
@@ -0,0 +1,29 @@
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return (mpg * fuelLeft >= distanceToPump)
+
+};
+
+
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+function SeriesSum(n) {
+    let sum = 0
+    for (let i = 0; i < n; i++) {
+        sum += 1 / (1 + i * 3)
+    }
+    return sum.toFixed(2)
+}
