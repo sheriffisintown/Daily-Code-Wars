4a03c747aa135a4f901989c3877e12630a9ce252
sheriffisintown <shshibli97@gmail.com>
2022-10-09 14:58:55 -0700

    add

---
+++ October/10-09-2022.js
@@ -0,0 +1,24 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+
+function friend(friends) {
+    return friends.filter(name => name.length === 4)
+}
+
+// Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+// Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them
+
+String.prototype.toJadenCase = function () {
+
+    return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
+};
